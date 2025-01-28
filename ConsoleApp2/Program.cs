56bb784eb32987fa46043290505930f3682c93d1
vamsinagavolu <vamsinagavolu@gmail.com>
2025-01-28 13:12:26 -0600

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("enter a number1");
+int number1;
+string userInput = Console.ReadLine();
+number1 = int.Parse(userInput);
+Console.WriteLine("enter a number2");
+int number2;
+string userInput1 = Console.ReadLine();
+number2 = int.Parse(userInput);
+int sum = number1 + number2;
+Console.WriteLine("sum of " + number1 + "and" + number2 + "is" + sum);
+Console.ReadKey();
